813eb6828a1c0e76246184b5e26864e24977be49
dcinachi1 <x22214895@student.ncirl.ie>
2023-04-12 19:54:33 +0100

    test

--- Extract.py
+++ Extract.py
@@ -140,4 +140,5 @@ def extract_Incident(start,db) -> FemaDataFrame:
 def stage_extracted_disasters(Fema):
     Fema.to_csv("staging/fema_disasters.csv",index=False,sep="\t")
 
+print('hello world')
 
